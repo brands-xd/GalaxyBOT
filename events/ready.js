ae96ebeaea668c900ea0b75213ea48046960bc75
HasanBrandsXD <hminecraft122@gmail.com>
2017-08-21 13:58:31 +0400

    Add files via upload

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,8 @@
 const config = require('../settings.json');
+const games = require('../games.json');
+
 
 module.exports = client => {
   console.log(`PROCESS ON ${client.guilds.size}`)
-  client.user.setGame(`&cmds | &invite [${client.shard.id}]`);
+  client.user.setPresence({ game: { name: `&cmds | &invite | [${client.shard.id}]`, type: 0 } });  
 };
